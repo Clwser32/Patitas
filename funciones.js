6b6d2af19349bc425eb800668cae17fdecdb3900
Clwser32 <165855703+Clwser32@users.noreply.github.com>
2024-04-30 01:45:51 -0300

    Categorias Agregadas-estructura de la pagina nueva-imagenes-logo

--- funciones.js
+++ funciones.js
@@ -1,8 +1,14 @@
 document.getElementById("showlogin").addEventListener("click", function () {
-    document.querySelector(".popup").classList.add("active");
+  document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".closebtn").addEventListener("click", function () {
+  document.querySelector(".popup").classList.remove("active");
+});
+//scroll automatico hacia el top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Desplazamiento suave
   });
-  
-  document.querySelector(".closebtn").addEventListener("click", function () {
-    document.querySelector(".popup").classList.remove("active");
-  });
-  
+}
